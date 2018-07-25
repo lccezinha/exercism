6c15252c774273b470e69ac084a63c2631d7c91d
Luiz Cezer <lccezinha@gmail.com>
2018-07-24 22:01:24 -0300

    Add World Count

---
+++ elixir/word-count/word_count.exs
@@ -0,0 +1,21 @@
+defmodule Words do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
+  def count(sentence) do
+    sentence
+      |> String.downcase
+      |> String.split(~r/[^[:alnum:]-]/u, trim: true) # Return only alphanumeric chars, more info: https://www.petefreitag.com/cheatsheets/regex/character-classes/
+      |> count_words
+  end
+
+  defp count_words(words) do
+    map = %{} # acc
+    Enum.reduce(words, map, fn(word, map) -> update_count(word, map) end)
+  end
+
+  defp update_count(word, map), do: Map.update(map, word, 1, &(&1 + 1)) # fn(n) -> n + 1 end
+end
